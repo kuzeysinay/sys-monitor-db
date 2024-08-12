f6e59eafe8daa4ae54938cd9b6084449082644c0
kuzeysinay <kuzeysinay@gmail.com>
2024-08-12 15:26:50 +0300

    scale graphs as y-axis is 0to100

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import psycopg2
+import altair as alt
 
 # PostgreSQL veritabanına bağlanma
 def get_data_from_postgres():
@@ -13,7 +14,7 @@ def get_data_from_postgres():
 # Streamlit uygulaması
 st.set_page_config(page_title="Sistem Bilgisi Panosu", layout="wide")  # Layout genişliği artırma
 
-st.title('🔍 Sistem Panosu')
+st.title('🔍 Sistem Bilgisi Panosu')
 st.write('Bu panel, sisteminizin CPU, RAM ve disk kullanımını gösterir.')
 
 # Veriyi çekme
@@ -26,11 +27,41 @@ data = data.sort_values(by='timestamp', ascending=False)
 st.write('### 📊 Son Sistem Verileri')
 st.dataframe(data, use_container_width=True)  # Tablonun genişliğini ayarla ve yatay kaydırma çubuğu ekle
 
+# CPU Kullanımı grafiği için altair kullanımı
 st.write('### 📈 CPU Kullanımı Zaman İçinde')
-st.line_chart(data[['timestamp', 'cpu_usage']].set_index('timestamp'), use_container_width=True)
+cpu_chart = alt.Chart(data).mark_line().encode(
+    x='timestamp:T',
+    y=alt.Y('cpu_usage:Q', scale=alt.Scale(domain=[0, 100])),
+    tooltip=['timestamp', 'cpu_usage']
+).properties(
+    width='container',
+    height=400
+).interactive()
 
+st.altair_chart(cpu_chart, use_container_width=True)
+
+# RAM Kullanımı grafiği için altair kullanımı
 st.write('### 🧠 RAM Kullanımı Zaman İçinde')
-st.bar_chart(data[['timestamp', 'ram_usage']].set_index('timestamp'), use_container_width=True)
+ram_chart = alt.Chart(data).mark_bar().encode(
+    x='timestamp:T',
+    y=alt.Y('ram_usage:Q', scale=alt.Scale(domain=[0, 100])),
+    tooltip=['timestamp', 'ram_usage']
+).properties(
+    width='container',
+    height=400
+).interactive()
 
+st.altair_chart(ram_chart, use_container_width=True)
+
+# Disk Kullanımı grafiği için altair kullanımı
 st.write('### 💽 Disk Kullanımı Zaman İçinde')
-st.bar_chart(data[['timestamp', 'disk_usage']].set_index('timestamp'), use_container_width=True)
+disk_chart = alt.Chart(data).mark_bar().encode(
+    x='timestamp:T',
+    y=alt.Y('disk_usage:Q', scale=alt.Scale(domain=[0, 100])),
+    tooltip=['timestamp', 'disk_usage']
+).properties(
+    width='container',
+    height=400
+).interactive()
+
+st.altair_chart(disk_chart, use_container_width=True)
